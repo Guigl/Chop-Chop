296fb9a83a463508a2b50314058c44702a1e424a
emerlgx <chouinard.joseph@gmail.com>
2016-01-29 20:17:15 -0500

    Added font and basic character
    LumberJack font for extra lumbrility

---
+++ Assets/Character_Stuff/Character.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+	public string charName = "Jack";
+
+	public float walkSpeed = 1.0f;
+	public float axePower = 1.0f;
+
+	public int backpackSize = 1;
+	public int lumberCount = 100;
+
+	public void loadCharacter (string n, float w, float a, int b, int l) {
+		// initialize character values
+		charName = n;
+		walkSpeed = w;
+		axePower = a;
+		backpackSize = b;
+		lumberCount = l;
+
+		// make the name display above their head
+		transform.GetComponentInChildren<TextMesh>().text = "Lumber" + charName;
+
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+		
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
