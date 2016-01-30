691f6d6a2cb777bb525b9fe5d1bbcab0f1b5cc61
emerlgx <chouinard.joseph@gmail.com>
2016-01-29 21:11:54 -0500

    Floating names are now more floaty
    float float

---
+++ Assets/Character_Stuff/LookAtCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour {
+	GameObject cam;
+
+	// Use this for initialization
+	void Start () {
+		cam = GameObject.FindGameObjectWithTag ("MainCamera");
+
+	}
+	
+	void Update () {
+		transform.LookAt(cam.transform.position);
+	}
+}
