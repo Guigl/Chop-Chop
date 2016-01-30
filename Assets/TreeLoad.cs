414fdf6efe5eb84587210472cc4ec642d6080efa
emerlgx <chouinard.joseph@gmail.com>
2016-01-29 22:47:13 -0500

    TreeReplace script for funtimes
    magically replaces terrain trees with prefab trees

---
+++ Assets/TreeLoad.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeLoad : MonoBehaviour {
+	public TerrainData terr;
+	public GameObject[] treeTypes;
+	// replace the terraintrees with prefab trees
+	void Start () {
+
+		// generate new trees
+		foreach (TreeInstance tree in terr.treeInstances) {
+			int treetype = tree.prototypeIndex;
+			Vector3 pos = new Vector3 (tree.position.x, tree.position.y, tree.position.z);
+			Instantiate ((GameObject)treeTypes[treetype], Vector3.Scale(pos, terr.size), Quaternion.identity);
+		}
+
+		// remove the terraintrees
+		TreeInstance[] temp = new TreeInstance[0];
+		terr.treeInstances = temp;
+	}
+
+}
